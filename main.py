13437325e9c1eef93eb135079125c88c5cfc894d
Your Name <you@example.com>
2022-03-21 10:25:46 +0500

    1 commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+from oxfordLookup import getDefinitions
+from googletrans import Translator
+translator = Translator()
+
+API_TOKEN = '5128968790:AAFbig93gBGtZ2oHuLIIJ0vZn_t0SDvgnrg'
+
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Salom, Hush kelibsiz!")\
+
+@dp.message_handler(commands=['help'])
+async def send_help(message: types.Message):
+    await message.reply("Savol va murojaat uchun: \n Admin: @aabubakr97")
+
+@dp.message_handler()
+async def tarjimon(message: types.Message):
+    lang = translator.detect(message.text).lang
+    if len(message.text.split()) > 2:
+        dest = 'uz' if lang == 'en' else 'en'
+        await message.reply(translator.translate(message.text, dest).text)
+
+    else:
+        if lang=='en':
+            word_id = message.text
+        else:
+            word_id = translator.translate(message.text, dest='en').text
+
+        lookup = getDefinitions(word_id)
+        if lookup:
+            await message.reply(f"Word:{word_id} \nDefinitions:\n{lookup['definitions']}")
+            if lookup.get('audio'):
+                await message.reply_voice(lookup['audio'])
+        else:
+            await message.reply("Bunday so'z topilmadi")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ oxfordLookup.py
@@ -0,0 +1,28 @@
+import requests
+app_id = "c2e7b1fe"
+app_key = "b41be5b3b08f91ebe3d849570f15c52a"
+language = "en-gb"
+
+def getDefinitions(word_id):
+    url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
+    r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+    res = r.json()
+    if 'error' in res.keys():
+        return False
+
+    output = {}
+    senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
+    definitions = []
+    for sense in senses:
+        definitions.append(f"👉 {sense['definitions'][0]}")
+    output['definitions'] = "\n".join(definitions)
+
+    if res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0].get('audioFile'):
+        output['audio'] = res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
+
+    return output
+
+if __name__ == '__main__':
+        from pprint import pprint as print
+        print(getDefinitions('Great Britain'))
+        print(getDefinitions('america'))
